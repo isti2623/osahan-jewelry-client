1ce460bfc60ed9e1717fb36ab45cb083488285df
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-10 18:31:12 +0600

    update

--- src/Pages/Admin/MakeAdmin/MakeAdmin.js
+++ src/Pages/Admin/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FormControl, InputGroup, Button } from 'react-bootstrap';
 
 const MakeAdmin = () => {
+    const [email, setEmail] = useState('');
+
+    const handleOnBlur = e => {
+        setEmail(e.target.value);
+    }
+
+    const handleAdminSubmit = e => {
+        e.preventDefault();
+    }
     return (
         <div>
             <h2>Make Admin</h2>
+            <form onSubmit={handleAdminSubmit}>
+                <InputGroup className="mb-3 w-25 my-5 ms-5">
+                    <FormControl
+                        type="email"
+                        placeholder="email"
+                        onBlur={handleOnBlur}
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                    />
+                    <Button variant="outline-secondary">
+                        Make Admin
+                    </Button>
+                </InputGroup>
+            </form>
         </div>
     );
 };
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -20,6 +20,7 @@ const useFirebase = () => {
                 setAuthError('');
                 const newUser = { email, displayName: name };
                 setUser(newUser);
+                saveUser(email, name);
                 // send name to firebase after creation
                 updateProfile(auth.currentUser, {
                     displayName: name
@@ -70,6 +71,18 @@ const useFirebase = () => {
             // An error happened.
         })
             .finally(() => setIsLoading(false));
+    }
+
+    const saveUser = (email, displayName) => {
+        const user = { email, displayName };
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then()
     }
 
     return {
