c8fba145363d01c03da0517dc3d55a3100bafaa3
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-10 18:47:21 +0600

    update admin role

--- src/Pages/Admin/MakeAdmin/MakeAdmin.js
+++ src/Pages/Admin/MakeAdmin/MakeAdmin.js
@@ -1,32 +1,48 @@
 import React, { useState } from 'react';
-import { FormControl, InputGroup, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const MakeAdmin = () => {
     const [email, setEmail] = useState('');
+    const [success, setSuccess] = useState(false);
+
 
     const handleOnBlur = e => {
         setEmail(e.target.value);
     }
 
     const handleAdminSubmit = e => {
-        e.preventDefault();
+        const user = { email };
+        fetch('http://localhost:5000/users', {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    console.log(data);
+                    setSuccess(true);
+
+                }
+            })
+
+        e.preventDefault()
     }
     return (
         <div>
             <h2>Make Admin</h2>
             <form onSubmit={handleAdminSubmit}>
-                <InputGroup className="mb-3 w-25 my-5 ms-5">
-                    <FormControl
-                        type="email"
-                        placeholder="email"
-                        onBlur={handleOnBlur}
-                        aria-label="Recipient's username"
-                        aria-describedby="basic-addon2"
-                    />
-                    <Button variant="outline-secondary">
-                        Make Admin
-                    </Button>
-                </InputGroup>
+                <input
+                    type="email"
+                    placeholder="email"
+                    onBlur={handleOnBlur} />
+
+                <Button type='submit' variant="danger">
+                    Make Admin
+                </Button>
+
             </form>
         </div>
     );
