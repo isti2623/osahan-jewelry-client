bac5a23a26a21bcc62b287a69d713bafcda78fe2
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-11 02:07:37 +0600

    final

--- src/Pages/Admin/ManageProducts/ManageProducts.js
+++ src/Pages/Admin/ManageProducts/ManageProducts.js
@@ -1,9 +1,46 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 const ManageProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [order, setOrder] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [products])
+
+    //DELETE AN Products
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Are you sure, you want to delete?');
+        if (proceed) {
+            const url = `http://localhost:5000/products/${id}`
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully');
+
+                        const remainingProducts = order.filter(order => order._id !== id);
+
+                        setOrder(remainingProducts);
+                    }
+                })
+        }
+
+    }
     return (
         <div>
-            <h2>Manage Products</h2>
+            {
+                products.map(product =>
+                    <div>
+                        <h1>{product.name}</h1>
+                        <p>{product.desc}</p>
+                        <button onClick={() => handleDeleteUser(product._id)} className="btn btn-danger m-2">Cancel</button>
+                    </div>
+
+                )
+            }
         </div>
     );
 };
