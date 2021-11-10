dc8d6c5149a8d377860ff1053d45815e243516de
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-10 15:21:03 +0600

    added post review

--- src/Pages/Dashboard/Review/Review.js
+++ src/Pages/Dashboard/Review/Review.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
 
 const Review = () => {
+    const { user } = useAuth();
+    const { register, reset, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+
+        data.email = user.email;
+        data.name = user.displayName;
+
+        fetch("http://localhost:5000/addReviews", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data) {
+                    alert('added successfully');
+                    reset();
+                }
+            });
+    };
+
     return (
         <div>
-            <h2>Review</h2>
+
+            <h2 className='custom-h1 text-center mb-3'>Add Your Review</h2>
+            <div className='form-part'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* register your input into the hook by invoking the "register" function */}
+                    <input required className='p-2 m-2' defaultValue={user.name} type='text' placeholder='name' {...register("name")} />
+                    <br />
+
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <input required className='p-2 m-2' defaultValue={user.email} type='email'  {...register("email")} />
+                    <br />
+                    <textarea required className='p-2 m-2' type='text' placeholder='description' {...register("desc")} />
+                    <br />
+                    {/* errors will return when field validation fails  */}
+                    {errors.exampleRequired && <span>This field is required</span>}
+                    <br />
+                    <input className='btn btn-warning ms-2' value='Add Review' type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
